646d85af7ec78fa6686743592a7d236a5d6d5feb
Ferdian Afza <farisafza201002@.com>
2020-01-19 13:55:46 +0700

    Change views in listing Article

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,7 +4,7 @@ class HomesController < ApplicationController
   before_action :set_article, only: [:show]
 
 	def index
-		@articles = Article.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
+		@articles = Article.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
 
 	end
 
